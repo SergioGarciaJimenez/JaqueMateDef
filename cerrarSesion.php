4ff411d8b29ad4dd0ff0dba8e9dfcc8bcada96bd
SergioGarciaJimenez <165771977+SergioGarciaJimenez@users.noreply.github.com>
2024-06-12 21:07:58 +0200

    Update cerrarSesion.php

--- cerrarSesion.php
+++ cerrarSesion.php
@@ -24,7 +24,7 @@ $_POST = []
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
   <link href="https://fonts.googleapis.com/css2?family=Besley:ital,wght@0,400..900;1,400..900&family=Kdam+Thmor+Pro&display=swap" rel="stylesheet" />
   <link href="https://fonts.googleapis.com/css2?family=Brygada+1918:ital,wght@0,400..700;1,400..700&display=swap" rel="stylesheet" />
-  <link rel="stylesheet" href="css/style.css" />
+<link rel="stylesheet" type="text/css" href="css/style.css?<?php echo time(); ?>" />
 </head>
 <!-- Cabecera -->
 <header>
