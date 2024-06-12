b304b22e07077d89e7686724d5423c9dcfd52c03
SergioGarciaJimenez <165771977+SergioGarciaJimenez@users.noreply.github.com>
2024-06-12 21:08:36 +0200

    Update registro.php

--- registro.php
+++ registro.php
@@ -30,7 +30,7 @@ if (isset($_SESSION['user'])) {
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
   <link href="https://fonts.googleapis.com/css2?family=Besley:ital,wght@0,400..900;1,400..900&family=Kdam+Thmor+Pro&display=swap" rel="stylesheet" />
   <link href="https://fonts.googleapis.com/css2?family=Brygada+1918:ital,wght@0,400..700;1,400..700&display=swap" rel="stylesheet" />
-  <link rel="stylesheet" href="css/style.css" />
+<link rel="stylesheet" type="text/css" href="css/style.css?<?php echo time(); ?>" />
   <!-- Importo Jquery -->
   <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
 
